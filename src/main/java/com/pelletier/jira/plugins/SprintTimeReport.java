a0beceb527715bd611ffc34fa4661632252ab14d
Pelletier <578993@bah.com>
2016-05-24 11:41:46 -0400

    Same function, now using JdbcTemplate! (As a side note, I am manually adding jars to Eclipse so that it doesn't whine at me for not having them, even though they will be provided by the system bundle (I think))

--- src/main/java/com/pelletier/jira/plugins/SprintTimeReport.java
+++ src/main/java/com/pelletier/jira/plugins/SprintTimeReport.java
@@ -10,69 +10,40 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class SprintTimeReport extends AbstractReport {
 	
 	
   private JdbcTemplate jdbcTemplate;
-  // JDBC driver name and database URL
-  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-  static final String DB_URL = "jdbc:mysql://localhost/jiradb";
 
-  //  Database credentials
-  static final String USER = "root";
-  static final String PASS = "test";
+  final String query = "SELECT AUTHOR as User, sum(timeworked) as TimeWorked FROM worklog WHERE STARTDATE > (select START_DATE from ao_60db71_sprint order by START_DATE DESC) GROUP BY AUTHOR ORDER BY TimeWorked DESC";
   
   public SprintTimeReport(JdbcTemplate jdbcTemplate){
 	  this.jdbcTemplate = jdbcTemplate;
-	  System.out.println(this.jdbcTemplate);
   }
 
    public String generateReportHtml(ProjectActionSupport projectActionSupport, Map map) throws Exception {
-   	Connection connection = null;
-   	Statement statement = null;
-   	ResultSet resultSet = null;
-   	StringBuilder stringBuilder = new StringBuilder();
+
    	List<UserTime> userTimes = new ArrayList<>();
-   	
+
    	try{
-   		Class.forName("com.mysql.jdbc.Driver");
-   		connection = DriverManager.getConnection(DB_URL, USER, PASS);
-   		statement = connection.createStatement();
-   		resultSet = statement.executeQuery("SELECT AUTHOR as User, sum(timeworked) as TimeWorked FROM worklog WHERE STARTDATE > (select START_DATE from ao_60db71_sprint order by START_DATE DESC) GROUP BY AUTHOR ORDER BY TimeWorked DESC");
-   		while(resultSet.next()){
-   			userTimes.add(new UserTime(resultSet.getString("User"), resultSet.getInt("TimeWorked")));
+   	    userTimes = jdbcTemplate.query(query, new RowMapper<UserTime>(){
+
+   		@Override
+   		public UserTime mapRow(ResultSet rs, int rowNum) throws SQLException {
+   			return new UserTime(rs.getString("User"), rs.getInt("TimeWorked"));
    		}
-   		
+   		   
+   	   });
    	}catch(Exception e){
    		e.printStackTrace();
-   		stringBuilder.append(e.getMessage());
    	}
+
    	
    	Map<String, Object> velocityParams = new HashMap<String, Object>();
    	velocityParams.put("userTimes",userTimes);
     return descriptor.getHtml("view", velocityParams);
    }
    
-   
-   public class UserTime{
-	   
-	   public String name;
-	   public int time;
-	   
-	   
-	   public UserTime(String name, int time){
-		   this.name = name;
-		   this.time = time;
-	   }
-	   
-	   public String getTime(){
-		   Integer hm = new Integer(time);
-		   return ((hm/60) + "hr" + (hm % 60) + "m ");
-	   }
-	   
-	   public String getName(){
-		   return this.name;
-	   }
-   }
 }
---
+++ src/main/java/com/pelletier/jira/plugins/UserTime.java
@@ -0,0 +1,22 @@
+package com.pelletier.jira.plugins;
+
+
+public class UserTime{
+	public String name;
+	public int time;
+
+
+	public UserTime(String name, int time){
+		   this.name = name;
+		   this.time = time;
+	}
+
+	public String getTime(){
+		   Integer hm = new Integer(time);
+		   return ((hm/60) + "hr" + (hm % 60) + "m ");
+	}
+
+	public String getName(){
+		   return this.name;
+	}
+}
