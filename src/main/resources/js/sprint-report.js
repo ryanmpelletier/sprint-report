47df6bcee6ead341388fef93da7c839317a36303
Pelletier <578993@bah.com>
2016-05-25 13:21:19 -0400

    Some changes, still incomplete

--- src/main/resources/js/sprint-report.js
+++ src/main/resources/js/sprint-report.js
@@ -3,31 +3,50 @@
 //probably some initialization stuff, like counting up an array of all the sprints
 
 //each sprint object will have an array of userTime objects
-var sprints = [];
-sprints = document.getElementsByName("sprint");
-for(var i = 0; i < sprints.length; i++){
 
-    //this is so later when we initialize we can set the first one to expanded
-    if(i == 0){
-        sprints[i].isExpanded = true;
-    }else{
-        sprints[i].isExpanded = false;
-    }
-    
-    sprints[i].userTimes = {};
+var sprints = [];
+
+window.onload = function(){
+	var sprints = document.getElementsByName("sprint");
+	for(var i = 0; i < sprints.length; i++){
+
+	    sprints[i].userTimes = [];
+
+	    //this is so later when we initialize we can set the first one to expanded
+	    if(i == 0){
+	        sprints[i].isExpanded = true;
+	    }else{
+	        sprints[i].isExpanded = false;
+	    }
+	}
+
+
+	var users = document.getElementsByName("user");
+	for(var i = 0; i < users.length; i++){
+		var sprintNumber = users[i].getAttribute("data-sprint-number");
+		sprints[sprintNumber - 1].userTimes.push(users[i].innerHTML);
+	}
+
+	for(var i = 0; i < sprints.length; i++){
+		alert(sprints[i].userTimes.length);
+		for(var j = 0; j < sprints[i].usersTimes.length; j++){
+			console.log(sprints[i].userTimes[j]);
+		}
+	}
 }
 
 
 //this function will probably take the id of a sprint, so that it can either expand or collapse the sprint
 function expandOrCollapse(sprintID){
     
-	if(sprints[sprintID].isExpanded){
+	if(sprints[sprintID - 1].isExpanded){
 		//collapse sprint
-		alert("Collapsing Sprint " + sprintID);
-		sprints[sprintID].isExpanded = false;
+		sprints[sprintID - 1].isExpanded = false;
+		alert(sprints[sprintID - 1].userTimes[0]);
 	}else{
 	    //expand sprint
-	    alert("Expanding Sprint " + sprintID);
-	    sprints[sprintID].isExpanded = true;
+	    sprints[sprintID - 1].isExpanded = true;
+		alert(sprints[sprintID - 1].userTimes[0]);
+	    
 	}
 }
