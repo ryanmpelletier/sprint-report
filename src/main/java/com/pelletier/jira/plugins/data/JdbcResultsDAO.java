8e2780d49037172158ab3e2ff55f1d0a41d2180e
Pelletier <578993@bah.com>
2016-06-06 07:38:47 -0400

    Add comment for future refactor

--- src/main/java/com/pelletier/jira/plugins/data/JdbcResultsDAO.java
+++ src/main/java/com/pelletier/jira/plugins/data/JdbcResultsDAO.java
@@ -40,6 +40,8 @@ public class JdbcResultsDAO implements ResultsDAO {
 		
 		
 		//unfortunately right now we rely on BasicDataSource because we can create it with url, driverClass, username, and password
+		//it would be better probably to use an interface with a createDataSource method, be able to write and inject an implementation
+		//that gets us the DataSource, I will ask about that later
 		BasicDataSource basicDataSource = new BasicDataSource();
 		
 		DbConfigReader dbConfigReader = new DbConfigReader();
