8e4e84d54b6f10bcddca2e4d8477656efdc77010
Pelletier <578993@bah.com>
2016-07-20 08:25:06 -0400

    Can now create a ValueListService object....I added the jar to my eclipse classpath, then to pom.xml, as well as META-INF/lib.

--- src/main/java/com/pelletier/jira/plugins/data/JdbcResultsDAO.java
+++ src/main/java/com/pelletier/jira/plugins/data/JdbcResultsDAO.java
@@ -3,15 +3,28 @@ package com.pelletier.jira.plugins.data;
 import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import com.pelletier.valuelist.ValueListService;
+import com.pelletier.valuelist.DefaultValuesListService;
+import com.pelletier.valuelist.DefaultValuesListService;
+import com.pelletier.valuelist.ValueListService;
 /*
  * Dependent on a list of queries, which will be run by the jdbcTemplate object.
  * 
  * Author: Ryan Pelletier
  */
 
-
+/**
+ * 
+ * @author 578993
+ *
+ *Ight so this is going to be changed, we are going to inject the valuelist service in here and
+ *then tell it what to do based on that, return our Values<> object, then somehow the view is oing
+ *to have to dynamically create a table...it'll work
+ */
 public class JdbcResultsDAO implements ResultsDAO {
 
 	private List<String> queries;
@@ -38,7 +51,7 @@ public class JdbcResultsDAO implements ResultsDAO {
 	//This function should do all the XML reading and stuff
 	public void setDbconfigLocation(String dbconfigLocation) {
 		
-		
+		ValueListService valueListService = new DefaultValuesListService<>();
 		//unfortunately right now we rely on BasicDataSource because we can create it with url, driverClass, username, and password
 		//it would be better probably to use an interface with a createDataSource method, be able to write and inject an implementation
 		//that gets us the DataSource, I will ask about that later
